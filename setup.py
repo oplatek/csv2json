583ffd202debe3589332e413c4eb122c5736ff81
Ondrej Platek <ondrej.platek@seznam.cz>
2016-07-29 23:55:52 +0200

    0.2 version

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
   name='csv2json',
   packages=['csv2json'],  # this must be the same as the name above
-  version='0.1',
+  version='0.2',
   description='A command-line tools and module to convert csv to json',
   author='Ondrej Platek',
   license='Apache 2.0',
   author_email='ondrej.platek.cz',
   url='https://github.com/oplatek/csv2json',  # use the URL to the github repo
-  download_url='https://github.com/oplatek/csv2json/tarball/0.1',  # I'll explain this in a second
+  download_url='https://github.com/oplatek/csv2json/tarball/0.2',  # I'll explain this in a second
   keywords=['csv', 'json', 'conversion'],
   scripts=['csv-to-json'],
   classifiers=[],
